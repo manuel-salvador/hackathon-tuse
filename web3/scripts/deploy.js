684c6bfc2beb895cced9d89699f1a597536e95d4
Marco Castiglione <marcocastiglione5@gmail.com>
2023-04-02 15:49:37 -0300

    web

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -6,17 +6,17 @@ async function main() {
   const tuseNFT = await ethers.getContractAt("TuseNFT", "0x6016f36eC97d3230Fd7abaa7899053362458F0A6");
 
   const ownerOf1 = await tuseNFT.ownerOf(2);
-  // const ownerOf2 = await tuseNFT.ownerOf(1);
-  // const ownerOf3 = await tuseNFT.ownerOf(2);
-  // const ownerOf4 = await tuseNFT.ownerOf(3);
-  // const ownerOf5 = await tuseNFT.ownerOf(4);
-
   console.log('ownerOf', ownerOf1)
-  // console.log('ownerOf', ownerOf2)
-  // console.log('ownerOf', ownerOf3)
-  // console.log('ownerOf', ownerOf4)
-  // console.log('ownerOf', ownerOf5)
+  
+  const tokenURI = await tuseNFT.tokenURI(2);
+  console.log('tokenURI', tokenURI)
 
 
+  const tusevault = await ethers.getContractAt('TuseVault', "0x1F63F399Ed8d54c4DCf989BE9F0362777f1c4861")
+
+  const ethInvested = await tusevault.getEthInvested(1);
+  console.log('ethInvested', ethInvested)
+
+  
 }
 main();
